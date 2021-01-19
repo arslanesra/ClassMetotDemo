188b1980798c55da098f724d9c41a38f6b79c7b7
Esra Arslan <arslnnesra@gmail.com>
2021-01-19 19:40:42 +0300

    Müşteri Takibi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi : " + musteri.Adi);
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi : " + musteri.Adi);
+        }
+
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi : " + musteri.Adi);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,33 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Esra";
+            musteri1.Soyadi = "Arslan";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Selim Mert";
+            musteri2.Soyadi = "Aydın";
+
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.List(musteri1);
+            musteriManager.List(musteri2);
+
+
+            Console.ReadLine();
+
         }
+
+       
     }
+
+
 }
